7c42fc0c05f8f61eb374c7ceb2d0311762e761e2
Mattias Ahlsén <mattias.ahlsen@gmail.com>
2024-12-30 14:43:30 +0100

    refactor: rename reduce to map-reduce

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,12 @@ export * from './find/findOrThrow.js';
 export * from './find/firstOrThrow.js';
 export * from './find/indexOrThrow.js';
 
-export * from './reduce/asyncFlatMap.js';
-export * from './reduce/asyncMap.js';
-export * from './reduce/asyncReduce.js';
-export * from './reduce/batchArray.js';
-export * from './reduce/batchArrayByWeights.js';
-export * from './reduce/batchAsyncIterableByWeights.js';
+export * from './map-reduce/asyncFlatMap.js';
+export * from './map-reduce/asyncMap.js';
+export * from './map-reduce/asyncReduce.js';
+export * from './map-reduce/batchArray.js';
+export * from './map-reduce/batchArrayByWeights.js';
+export * from './map-reduce/batchAsyncIterableByWeights.js';
 
 export * from './misc/createRange.js';
 export * from './misc/createSubject.js';
-
-export * from './reduce/asyncFlatMap.js';
-export * from './reduce/asyncMap.js';
-export * from './reduce/asyncReduce.js';
-export * from './reduce/batchArray.js';
-export * from './reduce/batchArrayByWeights.js';
-export * from './reduce/batchAsyncIterableByWeights.js';
